814ce8afe591cfe546a63fd903e95713820f0326
yuvrajsinghrana <yuvraj_r@hotmail.com>
2020-05-24 18:44:22 -0400

    Revert "Implementing Validation for Customers"
    This reverts commit a5cdd62e637b4e58083847505ba3d0baf3b17329.

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,13 +1,10 @@
-﻿using AutoMapper;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Vidly.App_Start;
 
 namespace Vidly
 {
@@ -15,8 +12,6 @@ namespace Vidly
     {
         protected void Application_Start()
         {
-            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
